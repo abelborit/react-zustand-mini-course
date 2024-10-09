2563dc68b018b9a641d125101d3019396e289c4a
Abel Borit <abelboritguitton@gmail.com>
2024-10-09 14:22:10 -0500

    [add] StateCreator

--- src/stores/person/person.store.ts
+++ src/stores/person/person.store.ts
@@ -1,4 +1,5 @@
-import { create } from "zustand";
+/* se coloca como -- type StateCreator -- porque solo usaremos el StateCreator como una interface y no queremos que cuando se cree la aplicación se importe nada ahí */
+import { create, type StateCreator } from "zustand";
 import { persist } from "zustand/middleware";
 
 /* se puede tener todo junto o sino por separado en dos interfaces */
@@ -20,25 +21,25 @@ interface ActionsState {
   setLastName: (value: string) => void;
 }
 
+/* al colocar middlewares una forma de mantener el orden puede ser colocar aparte nuestro objeto de configuración del store o sino también se puede colocar todo junto, ambas formas son válidas */
+const storeAPI: StateCreator<PersonState & ActionsState> = (set) => ({
+  firstName: "",
+  lastName: "",
+
+  /* FORMA 1: si no se usa el state igual se puede enviar o sino como función */
+  // setFirstName: (value: string) => set(() => ({ firstName: value })),
+  // setLastName: (value: string) => set((state) => ({ lastName: value })),
+
+  /* FORMA 2: como no se usa el state entonces se puede mandar directo ya que si se coloca como una función sin algún parámetro entonces es lo que recibe es lo mismo que se envía y por eso se puede colocar así */
+  setFirstName: (value: string) => set({ firstName: value }),
+  setLastName: (value: string) => set({ lastName: value }),
+});
+
 export const usePersonStore = create<PersonState & ActionsState>()(
   /* los middlewares son funciones que envuelven nuestro objeto de configuración del store, es decir, el -- (set => ({.....})) -- */
   /* este middleware persist nos servirá para grabar nuestro estado o state de forma persistente en el localStorage */
-  persist(
-    (set) => ({
-      firstName: "",
-      lastName: "",
-
-      /* FORMA 1: si no se usa el state igual se puede enviar o sino como función */
-      // setFirstName: (value: string) => set(() => ({ firstName: value })),
-      // setLastName: (value: string) => set((state) => ({ lastName: value })),
-
-      /* FORMA 2: como no se usa el state entonces se puede mandar directo ya que si se coloca como una función sin algún parámetro entonces es lo que recibe es lo mismo que se envía y por eso se puede colocar así */
-      setFirstName: (value: string) => set({ firstName: value }),
-      setLastName: (value: string) => set({ lastName: value }),
-    }),
-    {
-      /* este objeto adicional es para pasarle una configuración la cual llevará, por ejemplo, el name que será el nombre que se le dará al storage con el que quiero que se guarde en el localStorage */
-      name: "person-storage",
-    }
-  )
+  persist(storeAPI, {
+    /* este objeto adicional es para pasarle una configuración la cual llevará, por ejemplo, el name que será el nombre que se le dará al storage con el que quiero que se guarde en el localStorage */
+    name: "person-storage",
+  })
 );
