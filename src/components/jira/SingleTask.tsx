63d6de4352eabb08bd66cc12b5e86b9e7f102990
Abel Borit <abelboritguitton@gmail.com>
2024-10-10 13:37:21 -0500

    [add] dragging taks

--- src/components/jira/SingleTask.tsx
+++ src/components/jira/SingleTask.tsx
@@ -1,13 +1,24 @@
 import { IoReorderTwoOutline } from "react-icons/io5";
 import { TaskInterface } from "../../interfaces";
+import { useTaskStore } from "../../stores/taks/task.store";
 
 interface PropsInterface {
   task: TaskInterface;
 }
 
 export const SingleTask = ({ task }: PropsInterface) => {
+  const setDraggingTaskId = useTaskStore((state) => state.setDraggingTaskId);
+  const removeDraggingTaskId = useTaskStore(
+    (state) => state.removeDraggingTaskId
+  );
+
   return (
-    <div className="mt-5 flex items-center justify-between p-2">
+    <div
+      draggable
+      onDragStart={() => setDraggingTaskId(task.id)}
+      onDragEnd={removeDraggingTaskId}
+      className="mt-5 flex items-center justify-between p-2"
+    >
       <div className="flex items-center justify-center gap-2">
         <p className="text-base font-bold text-navy-700">{task.title}</p>
       </div>
--- src/stores/taks/task.store.ts
+++ src/stores/taks/task.store.ts
@@ -1,6 +1,7 @@
 /* se coloca como -- type StateCreator -- y -- type { TaskInterface, TaskStatus } -- porque solo usaremos el "StateCreator" y "TaskInterface, TaskStatus" como una interface y no queremos que cuando se cree la aplicación se importen algún archivo físico de las interfaces */
 import { create, type StateCreator } from "zustand";
 import type { TaskInterface, TaskStatus } from "../../interfaces";
+import { devtools } from "zustand/middleware";
 
 interface TaskState {
   /* FORMA 1: forma común de decir que será un objeto donde la key será de tipo string y el value será de tipo TaskInterface */
@@ -8,8 +9,11 @@ interface TaskState {
 
   /* FORMA 2: usando Record propio de TypeScript el cual es un genérico que dice que la key será de tipo string y el value será de tipo TaskInterface */
   tasks: Record<string, TaskInterface>;
+  draggingTaskId?: string; // será opcional porque en un momento tendré undefined y luego en otro momento tendré el valor el cual será el id de la tarea que quiero mover
 
   getTaskByStatus: (status: TaskStatus) => TaskInterface[];
+  setDraggingTaskId: (taskId: string) => void; // cuando se quiere hacer el dragging de la tarea
+  removeDraggingTaskId: () => void; //  cuando se suelta la tarea el estado final tiene que ser como cuando empezó "undefined" porque ya se completó el movimiento de la tarea ya sea en el mismo lugar o a otro lado
 }
 
 /* al colocar middlewares una forma de mantener el orden puede ser colocar aparte nuestro objeto de configuración del store o sino también se puede colocar todo junto, ambas formas son válidas */
@@ -21,6 +25,7 @@ const storeAPI: StateCreator<TaskState> = (set, get) => ({
     "ABC-3": { id: "ABC-3", title: "Task 3", status: "open" },
     "ABC-4": { id: "ABC-4", title: "Task 4", status: "open" },
   },
+  draggingTaskId: undefined, // no es necesario colocarlo explícitamente porque es opcional y si no está entonces es undefined por defecto, pero se coloca para saber que ahí tenemos esas propiedad y que quede un poco más explícito el código
 
   getTaskByStatus: (status: TaskStatus) => {
     const tasks = get().tasks;
@@ -28,7 +33,15 @@ const storeAPI: StateCreator<TaskState> = (set, get) => ({
     /* Object.values(tasks) para barrer todos los objetos por sus values y eso me retorna un arreglo y ahí recién puedo hacer uso de los métodos de los arreglos como el map, some, filter, etc */
     return Object.values(tasks).filter((task) => task.status === status);
   },
+
+  setDraggingTaskId: (taskId: string) => {
+    set({ draggingTaskId: taskId });
+  },
+
+  removeDraggingTaskId: () => {
+    set({ draggingTaskId: undefined });
+  },
 });
 
 /* lo que sí es importante al crear un store es que se coloca como un hook personalizado, es decir, con la palabra "use" y las reglas de React dicen que todos los hooks tienen que empezar con la palabra "use" */
-export const useTaskStore = create<TaskState>()(storeAPI);
+export const useTaskStore = create<TaskState>()(devtools(storeAPI));
