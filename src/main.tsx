ef2b27ded4015167fbf1f03245f84f83d8374c05
Abel Borit <abelboritguitton@gmail.com>
2024-10-16 10:32:28 -0500

    [add] QueryClientProvider & QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.tsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from './utils/queryClient.ts';
 
+/* al trabajar con React Query se hace uso de un Provider y usará también el cliente de React Query que ya hemos creado */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={ router } />
+    <QueryClientProvider client={ queryClient }>
+      <RouterProvider router={ router } />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
---
+++ src/utils/queryClient.ts
@@ -0,0 +1,7 @@
+/* configuración básica para usar el cliente de React Query */
+import { QueryClient } from "@tanstack/react-query";
+
+/* instanciar la clase QueryClient */
+const queryClient = new QueryClient();
+
+export default queryClient;
