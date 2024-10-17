4414a644d4adb127303c9a9499597b671dd1d26c
Abel Borit <abelboritguitton@gmail.com>
2024-10-17 00:10:13 -0500

    [add] useFavoriteReposGithubStore in ReactQueryPage & CardReposGithub

--- src/components/reactQuery/CardReposGithub.tsx
+++ src/components/reactQuery/CardReposGithub.tsx
@@ -1,11 +1,27 @@
 import { WhiteCard } from "../shared/cards/WhiteCard";
 import { Repository } from "../../interfaces/github.interface";
+import { useFavoriteReposGithubStore } from "../../stores/favoriteReposGithub/favoriteReposGithub.store";
 
 interface PropsInterface {
   repository: Repository;
+  isFavorite: boolean;
 }
 
-export const CardReposGithub = ({ repository }: PropsInterface) => {
+export const CardReposGithub = ({ repository, isFavorite }: PropsInterface) => {
+  const addFavoriteRepo = useFavoriteReposGithubStore(
+    (state) => state.addFavoriteRepo
+  );
+
+  const removeFavoriteRepo = useFavoriteReposGithubStore(
+    (state) => state.removeFavoriteRepo
+  );
+
+  const handleToggleFavorite = () => {
+    isFavorite
+      ? removeFavoriteRepo(repository.id)
+      : addFavoriteRepo(repository.id);
+  };
+
   return (
     <WhiteCard centered>
       <h2>{repository.name}</h2>
@@ -22,6 +38,11 @@ export const CardReposGithub = ({ repository }: PropsInterface) => {
         Default Branch:{" "}
         <span className="font-normal">{repository.default_branch}</span>
       </p>
+      <br />
+
+      <button onClick={handleToggleFavorite}>
+        {isFavorite ? "Dislike" : "Like"}
+      </button>
     </WhiteCard>
   );
 };
--- src/pages/04-react-query/ReactQueryPage.tsx
+++ src/pages/04-react-query/ReactQueryPage.tsx
@@ -1,9 +1,15 @@
 import { CardReposGithub } from "../../components/reactQuery/CardReposGithub";
 import { useFetchRepositories } from "../../hooks/useReposGithub";
+import { useFavoriteReposGithubStore } from "../../stores/favoriteReposGithub/favoriteReposGithub.store";
 
 export const ReactQueryPage = () => {
   const { data, isLoading } = useFetchRepositories();
   // console.log({ data });
+
+  const favoriteReposGithub = useFavoriteReposGithubStore(
+    (state) => state.favoriteReposIds
+  );
+  // console.log(favoriteReposGithub);
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -14,8 +20,12 @@ export const ReactQueryPage = () => {
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-        {data?.map((element) => (
-          <CardReposGithub repository={element} />
+        {data?.map((repository) => (
+          <CardReposGithub
+            key={repository.id}
+            repository={repository}
+            isFavorite={favoriteReposGithub.includes(repository.id)} // dará un true o un false
+          />
         ))}
       </div>
     </>
